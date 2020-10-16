086e9cdf90f00ec4f44ba5c1568f9c40618424d6
Fernando Augusto <wfernandoaugusto01@gmail.com>
2020-10-15 23:41:38 -0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import {
   SafeAreaView,
@@ -8,48 +8,90 @@ import {
   StatusBar,
   StyleSheet,
   TouchableOpacity,
+  Linking
 } from "react-native";
+
+import api from './services/api'
+
+const Repository = ({ id, title, url, techs = [], likes, handleLikeRepository }) => {
+  return (
+    <View style={styles.repositoryContainer}>
+      <Text style={styles.repository}>{title}</Text>
+
+      <Text onPress={() => Linking.openURL(url)} style={styles.link}>URL: {url}</Text>
+
+      <View style={styles.techsContainer}>
+        {
+          techs.map(tech => (
+            <Text key={tech} style={styles.tech}>
+              {tech}
+            </Text>
+          ))
+        }
+      </View>
+
+      <View style={styles.likesContainer}>
+        <Text
+          style={styles.likeText}
+          testID={`repository-likes-${id}`}
+        >
+          {likes} curtidas
+        </Text>
+      </View>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={(id) => handleLikeRepository(id)}
+        // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+        testID={`like-button-${id}`}
+      >
+        <Text style={styles.buttonText}>Curtir</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
 
 export default function App() {
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    const response = await api.post(`repositories/${id}/like`)
+
+    const likedRepoIndex = repositories.findIndex(repo => repo.id === response.data.id)
+
+    const newList = [...repositories]
+    newList[likedRepoIndex] = response.data
+
+    setRepositories(newList)
   }
+
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('/repositories')
+      .then((response) => setRepositories(response.data))
+      .catch((error) => console.warn(error))
+  }, [])
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
-
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
-
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+        <FlatList
+          data={repositories}
+          keyExtractor={repo => repo.id}
+          renderItem={({ item: repo }) => {
+            return (
+              <Repository
+                id={repo.id}
+                title={repo.title}
+                url={repo.url}
+                techs={repo.techs}
+                likes={repo.likes}
+                handleLikeRepository={() => handleLikeRepository(repo.id)}
+              />
+            )
+          }}
+        />
       </SafeAreaView>
     </>
   );
@@ -104,4 +146,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#7159c1",
     padding: 15,
   },
+  link: {
+    fontWeight: 'bold',
+    color: '#00f',
+    textDecorationLine: 'underline'
+  }
 });
